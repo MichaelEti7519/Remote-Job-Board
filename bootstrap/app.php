4c9ae7b74bb61f33af31af38affa1fce70d06e70
MichaelEti7519 <michealeti2006@gmail.com>
2026-07-03 17:28:15 +0100

    Changes to be committed:

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Middleware;
-use Illuminate\Foundation\Http\Middleware\ValidateSignature;  // ✅ fixed
+use Illuminate\Foundation\Http\Middleware\ValidateSignature;  
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
